4286b90c14e85775b5e2a70f3c8486497239e781
102782 <fuyajyoured@yahoo.co.jp>
2014-02-20 10:35:41 +0900

    IPluginメンバの用意

--- JoyInputer/Plugin_VJoyInputer.cs
+++ JoyInputer/Plugin_VJoyInputer.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
+using System.Threading;
+using System.ComponentModel;
+using System.Windows.Forms;
+using FNF.Utility;
+using FNF.Controls;
+using FNF.XmlSerializerSetting;
+using FNF.BouyomiChanApp;
 
 namespace JoyInputer
 {
-    public class Plugin_VJoyInputer
+    public class Plugin_VJoyInputer : IPlugin 
     {
+        public string Name { get { return ""; } }
+
+        public string Version { get { return ""; } }
+
+        public string Caption { get { return ""; } }
+
+        public ISettingFormData SettingFormData { get { return null; } } //プラグインの設定画面情報（設定画面が必要なければnullを返してください）
+
+        //プラグイン開始時処理
+        public void Begin()
+        {
+
+        }
+
+        //プラグイン終了時処理
+        public void End()
+        {
+            
+        }
     }
 }
